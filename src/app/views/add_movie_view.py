399fc94e3cff35ac98bb1346e0dc6e7961c077bc
Nilrudra1999 <nilrudram@gmail.com>
2026-02-26 23:25:14 -0500

    minor updates - workflow change and py code update

--- src/app/views/add_movie_view.py
+++ src/app/views/add_movie_view.py
@@ -5,11 +5,20 @@
     Email: nilrudram@gmail.com
     Github: github.com/Nilrudra1999
 ------------------------------------------------------------------------------------------------"""
-class AddMovieView():
+from customtkinter import CTkFrame
+
+BG_COLOR_ALL = "transparent"
+TEXT_PRIMARY_COLOR = "#b01756"
+TEXT_SECONDARY_COLOR = "#b42b68"
+TILE_FOCUS_COLOR = "#292450"
+
+
+class AddMovieView(CTkFrame):
     """
     Add movie UI class inherited from Custom Tkinter Frame class, provides a collection of UI 
     elements which allow users to add movies to their local database. The view contains a form 
     to collect movie information and will auto-alert if any of the info entered is "incorrect".
     """
     def __init__(self, controller, window) -> None:
-        pass
+        super().__init__(window, fg_color=BG_COLOR_ALL)
+        self.controller = controller
--- src/app/views/predictor_view.py
+++ src/app/views/predictor_view.py
@@ -5,11 +5,20 @@
     Email: nilrudram@gmail.com
     Github: github.com/Nilrudra1999
 ------------------------------------------------------------------------------------------------"""
-class PredictorView():
+from customtkinter import CTkFrame
+
+BG_COLOR_ALL = "transparent"
+TEXT_PRIMARY_COLOR = "#b01756"
+TEXT_SECONDARY_COLOR = "#b42b68"
+TILE_FOCUS_COLOR = "#292450"
+
+
+class PredictorView(CTkFrame):
     """
     Predictor UI class inherited from Custom Tkinter Frame class, contains UI elements for 
     predicting the success of movies when presented with information. The view contains a form 
     section to accept movie info and data visualization zone to display predictive analytics.
     """
     def __init__(self, controller, window) -> None:
-        pass
+        super().__init__(window, fg_color=BG_COLOR_ALL)
+        self.controller = controller
